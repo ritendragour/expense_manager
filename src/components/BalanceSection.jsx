d8e951f272ff0efc465f02a9d14c9457c93c75d3
Ritendra <ritendra.gour@clickspurr.com>
2023-06-25 01:11:33 +0530

    note added

--- src/components/BalanceSection.jsx
+++ src/components/BalanceSection.jsx
@@ -68,6 +68,9 @@ const BalanceSection = () => {
         <button>
           Save Transaction<i className="fa-solid fa-floppy-disk"></i>
         </button>
+        <div className="w-100">
+        <p className="notess">You want to subtract transactions. so please use the minus sign (-)</p>
+        </div>
       </form>
     </div>
   );
